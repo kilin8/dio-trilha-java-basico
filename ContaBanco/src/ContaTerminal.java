25ed21df1ddc04c3dd8b8093e9add3f6b4e13405
Franklin Nunes <franklinnunis@gmail.com>
2023-09-12 23:21:05 -0300

    implementando atividade

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.US);
+        
+        System.out.println("Digite o número da sua conta:");
+        int conta = input.nextInt();
+        input.nextLine();
+        System.out.println("Digite a sua agência:");
+        String agencia = input.nextLine();
+        System.out.println("Digite o seu nome:");
+        String nomeCliente = input.nextLine();
+        System.out.println("Digite seu saldo:");
+        double saldo = input.nextDouble();
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " +  conta + " e seu saldo de " + saldo + " já está disponível para saque.");
+        
     }
 }
